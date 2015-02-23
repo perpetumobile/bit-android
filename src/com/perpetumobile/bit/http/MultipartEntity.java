13e3ab6c0aed664b15a7c5bb3082cebc647bd8d1
zdukic <zdukic@gmail.com>
2015-02-23 14:45:44 -0800

    pass InputStream to DataPart instead of ByteBuffer

--- src/com/perpetumobile/bit/http/MultipartEntity.java
+++ src/com/perpetumobile/bit/http/MultipartEntity.java
@@ -1,10 +1,11 @@
 package com.perpetumobile.bit.http;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
 import com.perpetumobile.bit.util.Logger;
@@ -22,24 +23,61 @@ class FormPart {
 
 class DataPart {
 	protected String partName = null;
-	protected String contentType = null;
 	protected String fileName = null;
-	protected byte[] data = null;
+	protected int contentLength = 0;
+	protected String contentType = null;
+	protected InputStream contentStream = null;
 
-	public DataPart(String partName, String contentType, String fileName, byte[] data) {
+	public DataPart(String partName, String fileName, int contentLength, String contentType, InputStream contentStream) {
 		this.partName = partName;
-		this.contentType = contentType;
 		this.fileName = fileName;
-		this.data = data;
+		this.contentLength = contentLength;
+		this.contentType = contentType;
+		this.contentStream = contentStream;
+	}
+	
+	public DataPart(String partName, String contentType, File file) throws IOException {
+		this.partName = partName;
+		this.fileName = file.getName();
+		this.contentLength = (int)file.length();
+		this.contentType = contentType;
+		this.contentStream = new FileInputStream(file);
+	}
+	
+	public void writeFileName(OutputStream os) throws IOException {
+		if(!Util.nullOrEmptyString(fileName)) {
+			os.write(MultipartEntity.filenameKey);
+			os.write(fileName.getBytes());
+			os.write(MultipartEntity.quote);
+		}
+	}
+	
+	public void writeContentLength(OutputStream os) throws IOException {
+		if(contentLength > 0) {
+			os.write(MultipartEntity.crlf);
+			os.write(MultipartEntity.contentLengthKey);
+			os.write(Integer.toString(contentLength).getBytes());
+		}
 	}
 	
 	public void writeContentType(OutputStream os) throws IOException {
+		os.write(MultipartEntity.crlf);
 		if(Util.nullOrEmptyString(contentType)) {
 			os.write(MultipartEntity.contentTypeApplicationOctetStream);
 		} else {
 			os.write(MultipartEntity.contentTypeKey);
 			os.write(contentType.getBytes());
 		}
+	}
+	
+	public void writeContent(OutputStream os) throws IOException {
+		os.write(MultipartEntity.crlf);
+		byte[] buf = new byte[1024];
+		int len = 0;
+		while ((len = contentStream.read(buf)) != -1) {
+			os.write(buf, 0, len);
+		}
+		contentStream.close();
 	}
 }
 
@@ -76,13 +114,20 @@ public class MultipartEntity {
 		formParts.add(new FormPart(partName, value));
 	}
 	
-	public void addFilePart(String partName, String contentType, File file) throws IOException {
-		ByteBuffer data = Util.readBinaryFile(file);
-		dataParts.add(new DataPart(partName, contentType, file.getName(), data.array()));
+	public void addDataPart(String partName, InputStream contentStream) {
+		dataParts.add(new DataPart(partName, null, 0, null, contentStream));
 	}
 	
-	public void addDataPart(String partName, String contentType, String fileName, byte[] data) {
-		dataParts.add(new DataPart(partName, contentType, fileName, data));
+	public void addDataPart(String partName, String contentType, InputStream contentStream) {
+		dataParts.add(new DataPart(partName, null, 0, contentType, contentStream));
+	}
+	
+	public void addDataPart(String partName, String fileName, int contentLength, String contentType, InputStream contentStream) {
+		dataParts.add(new DataPart(partName, fileName, contentLength, contentType, contentStream));
+	}
+	
+	public void addDataPart(String partName, String contentType, File file) throws IOException {
+		dataParts.add(new DataPart(partName, contentType, file));
 	}
 	
 	static public void prepareConnection(HttpURLConnection c) {
@@ -119,19 +164,13 @@ public class MultipartEntity {
 		os.write(contentDispositionFormData_nameKey);
 		os.write(part.partName.getBytes());
 		os.write(quote);
-		os.write(filenameKey);
-		os.write(part.fileName.getBytes());
-		os.write(quote);
-		os.write(crlf);
+		part.writeFileName(os);
 		part.writeContentType(os);
-		os.write(crlf);
-		os.write(contentLengthKey);
-		os.write(Integer.toString(part.data.length).getBytes());
+		part.writeContentLength(os);
 		os.write(crlf);
 		os.write(contentTransferEncodingBinary);
 		os.write(crlf);
-		os.write(crlf);
-		os.write(part.data);
+		part.writeContent(os);
 		os.write(crlf);
 	}
 	
